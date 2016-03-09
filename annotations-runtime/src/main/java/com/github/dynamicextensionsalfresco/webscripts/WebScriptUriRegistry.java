569a7d38fa0018f33608e18b48ce2ec4c1868122
Arnas Petraitis <arnas.petraitis@visma.com>
2016-03-09 14:11:27 +0200

    warn when webscript family is empty

--- annotations-runtime/src/main/java/com/github/dynamicextensionsalfresco/webscripts/WebScriptUriRegistry.java
+++ annotations-runtime/src/main/java/com/github/dynamicextensionsalfresco/webscripts/WebScriptUriRegistry.java
@@ -1,6 +1,7 @@
 package com.github.dynamicextensionsalfresco.webscripts;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -19,202 +20,197 @@ import org.springframework.extensions.webscripts.Registry;
 import org.springframework.extensions.webscripts.UriIndex;
 import org.springframework.extensions.webscripts.WebScript;
 import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 
 /**
  * Registers {@link WebScript}s in an {@link UriIndex}.
  * <p>
  * This implementation wraps and replaces the {@link UriIndex} bean, allowing {@link WebScript}s to be registered
  * independently of the existing {@link Registry}.
- * 
+ *
  * @author Laurens Fridael
- * 
+ *
  */
 public class WebScriptUriRegistry implements UriIndex, BeanPostProcessor, InitializingBean {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	/* Dependencies */
+    /* Dependencies */
+    private UriIndex uriIndex;
 
-	private UriIndex uriIndex;
+    private WebScript unavailableWebScript = new UnavailableWebScript();
 
-	private WebScript unavailableWebScript = new UnavailableWebScript();
+    /* Configuration */
+    private String uriIndexBeanName;
 
-	/* Configuration */
+    /* State */
+    private final List<WebScript> webScripts = new ArrayList<WebScript>();
 
-	private String uriIndexBeanName;
+    private final Map<UriMethod, WebScriptProxy> webScriptProxiesByUriMethod = new HashMap<UriMethod, WebScriptProxy>();
 
-	/* State */
+    private final Lock lock = new ReentrantLock();
 
-	private final List<WebScript> webScripts = new ArrayList<WebScript>();
+    /* Main operations */
+    /**
+     * Registers a {@link WebScript} with the {@link UriIndex}.
+     *
+     * @param webScript
+     */
+    public void registerWebScript(final WebScript webScript) {
+        lock.lock();
+        try {
+            if (uriIndex != null) {
+                doRegisterWebScript(webScript);
+            } else if (logger.isDebugEnabled()) {
+                logger.debug("Delaying registration of Web Script '{}' until UriIndex is available.", webScript
+                        .getDescription().getId());
+            }
+            webScripts.add(webScript);
+        } finally {
+            lock.unlock();
+        }
+    }
 
-	private final Map<UriMethod, WebScriptProxy> webScriptProxiesByUriMethod = new HashMap<UriMethod, WebScriptProxy>();
+    /**
+     * Unregisters a WebScript.
+     * <p>
+     * As the {@link UriIndex} has no unregistration methods, the actual URI binding will only be cleared when the
+     * {@link Registry} is reset.
+     *
+     * @param webScript
+     */
+    public void unregisterWebScript(final WebScript webScript) {
+        lock.lock();
+        try {
+            doUnregisterWebScript(webScript);
+        } finally {
+            lock.unlock();
+        }
+    }
 
-	private final Lock lock = new ReentrantLock();
+    /**
+     * Clears the {@link UriIndex} and registers pending {@link WebScript}s.
+     */
+    @Override
+    public void clear() {
+        Assert.state(uriIndex != null);
+        lock.lock();
+        try {
+            uriIndex.clear();
+            webScriptProxiesByUriMethod.clear();
+            registerPendingWebScripts();
+        } finally {
+            lock.unlock();
+        }
+    }
 
-	/* Main operations */
+    /* Callback operations */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //Assert.state(StringUtils.hasText(uriIndexBeanName), "Name of UriIndex bean not configured.");
+    }
 
-	/**
-	 * Registers a {@link WebScript} with the {@link UriIndex}.
-	 * 
-	 * @param webScript
-	 */
-	public void registerWebScript(final WebScript webScript) {
-		lock.lock();
-		try {
-			if (uriIndex != null) {
-				doRegisterWebScript(webScript);
-			} else {
-				if (logger.isDebugEnabled()) {
-					logger.debug("Delaying registration of Web Script '{}' until UriIndex is available.", webScript
-							.getDescription().getId());
-				}
-			}
-			webScripts.add(webScript);
-		} finally {
-			lock.unlock();
-		}
-	}
+    @Override
+    public Object postProcessBeforeInitialization(final Object bean, final String beanName) throws BeansException {
+        return bean;
+    }
 
-	/**
-	 * Unregisters a WebScript.
-	 * <p>
-	 * As the {@link UriIndex} has no unregistration methods, the actual URI binding will only be cleared when the
-	 * {@link Registry} is reset.
-	 * 
-	 * @param webScript
-	 */
-	public void unregisterWebScript(final WebScript webScript) {
-		lock.lock();
-		try {
-			doUnregisterWebScript(webScript);
-		} finally {
-			lock.unlock();
-		}
-	}
+    @Override
+    public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
+        if (this.uriIndexBeanName.equals(beanName) && bean instanceof UriIndex) {
+            setUriIndex((UriIndex) bean);
+            return this;
+        } else {
+            return bean;
+        }
+    }
 
-	/**
-	 * Clears the {@link UriIndex} and registers pending {@link WebScript}s.
-	 */
-	@Override
-	public void clear() {
-		Assert.state(uriIndex != null);
-		lock.lock();
-		try {
-			uriIndex.clear();
-			webScriptProxiesByUriMethod.clear();
-			registerPendingWebScripts();
-		} finally {
-			lock.unlock();
-		}
-	}
+    public List<WebScript> getWebScripts() {
+        return webScripts;
+    }
 
-	/* Callback operations */
+    public Set<String> getWebScriptFamilies() {
+        Set<String> allFamilies = new HashSet<>();
+        webScripts.stream()
+                .map(webScript -> {
+                    if (webScript.getDescription().getFamilys() == null) {
+                        logger.warn("Webscript familly is missing: {}", webScript.getDescription().getId());
+                        return Collections.<String>emptySet();
+                    }
+                    return webScript.getDescription().getFamilys();
+                })
+                .forEach(allFamilies::addAll);
+        return allFamilies;
+    }
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		//Assert.state(StringUtils.hasText(uriIndexBeanName), "Name of UriIndex bean not configured.");
-	}
+    /* Remaining delegated operations */
+    @Override
 
-	@Override
-	public Object postProcessBeforeInitialization(final Object bean, final String beanName) throws BeansException {
-		return bean;
-	}
+    public int getSize() {
+        return uriIndex.getSize();
+    }
 
-	@Override
-	public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
-		if (this.uriIndexBeanName.equals(beanName) && bean instanceof UriIndex) {
-			setUriIndex((UriIndex) bean);
-			return this;
-		} else {
-			return bean;
-		}
-	}
+    @Override
+    public void registerUri(final WebScript script, final String uri) {
+        uriIndex.registerUri(script, uri);
+    }
 
-	public List<WebScript> getWebScripts() {
-		return webScripts;
-	}
-	
-	public Set<String> getWebScriptFamilies(){
-	    Set<String> allFamilies = new HashSet<String>();
-	    webScripts.stream()
-	        .map(webScript -> webScript.getDescription().getFamilys())
-	        .forEach(wsFamilies -> allFamilies.addAll(wsFamilies));
-	    return allFamilies;
-	}
-	
-	/* Remaining delegated operations */
+    @Override
+    public Match findWebScript(final String method, final String uri) {
+        return uriIndex.findWebScript(method, uri);
+    }
 
-	@Override
-	public int getSize() {
-		return uriIndex.getSize();
-	}
+    /* Utility operations */
+    private void doRegisterWebScript(final WebScript webScript) {
+        Assert.state(uriIndex != null);
+        for (final String uri : webScript.getDescription().getURIs()) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Registering Web Script '{}' for URI '{}'", webScript.getDescription().getId(), uri);
+            }
+            final WebScriptProxy webScriptProxy;
+            final UriMethod key = UriMethod.forUriAndMethod(uri, webScript.getDescription().getMethod());
+            if (webScriptProxiesByUriMethod.containsKey(key) == false) {
+                webScriptProxy = new WebScriptProxy(webScript);
+                webScriptProxiesByUriMethod.put(key, webScriptProxy);
+            } else {
+                webScriptProxy = webScriptProxiesByUriMethod.get(key);
+                webScriptProxy.setWebScript(webScript);
+            }
+            uriIndex.registerUri(webScriptProxy, uri);
+        }
+    }
 
-	@Override
-	public void registerUri(final WebScript script, final String uri) {
-		uriIndex.registerUri(script, uri);
-	}
+    private void doUnregisterWebScript(final WebScript webScript) {
+        webScripts.remove(webScript);
+        for (final String uri : webScript.getDescription().getURIs()) {
+            final UriMethod key = UriMethod.forUriAndMethod(uri, webScript.getDescription().getMethod());
+            if (webScriptProxiesByUriMethod.containsKey(key)) {
+                final WebScriptProxy webScriptProxy = webScriptProxiesByUriMethod.get(key);
+                webScriptProxy.setWebScript(unavailableWebScript);
+            }
+        }
+    }
 
-	@Override
-	public Match findWebScript(final String method, final String uri) {
-		return uriIndex.findWebScript(method, uri);
-	}
+    private void registerPendingWebScripts() {
+        for (final WebScript webScript : webScripts) {
+            doRegisterWebScript(webScript);
+        }
+    }
 
-	/* Utility operations */
+    /* Dependencies */
+    public void setUriIndex(final UriIndex uriIndex) {
+        Assert.notNull(uriIndex);
+        this.uriIndex = uriIndex;
+    }
 
-	private void doRegisterWebScript(final WebScript webScript) {
-		Assert.state(uriIndex != null);
-		for (final String uri : webScript.getDescription().getURIs()) {
-			if (logger.isDebugEnabled()) {
-				logger.debug("Registering Web Script '{}' for URI '{}'", webScript.getDescription().getId(), uri);
-			}
-			final WebScriptProxy webScriptProxy;
-			final UriMethod key = UriMethod.forUriAndMethod(uri, webScript.getDescription().getMethod());
-			if (webScriptProxiesByUriMethod.containsKey(key) == false) {
-				webScriptProxy = new WebScriptProxy(webScript);
-				webScriptProxiesByUriMethod.put(key, webScriptProxy);
-			} else {
-				webScriptProxy = webScriptProxiesByUriMethod.get(key);
-				webScriptProxy.setWebScript(webScript);
-			}
-			uriIndex.registerUri(webScriptProxy, uri);
-		}
-	}
+    public void setUnavailableWebScript(final WebScript unavailableWebScript) {
+        Assert.notNull(unavailableWebScript);
+        this.unavailableWebScript = unavailableWebScript;
+    }
 
-	private void doUnregisterWebScript(final WebScript webScript) {
-		webScripts.remove(webScript);
-		for (final String uri : webScript.getDescription().getURIs()) {
-			final UriMethod key = UriMethod.forUriAndMethod(uri, webScript.getDescription().getMethod());
-			if (webScriptProxiesByUriMethod.containsKey(key)) {
-				final WebScriptProxy webScriptProxy = webScriptProxiesByUriMethod.get(key);
-				webScriptProxy.setWebScript(unavailableWebScript);
-			}
-		}
-	}
-
-	private void registerPendingWebScripts() {
-		for (final WebScript webScript : webScripts) {
-			doRegisterWebScript(webScript);
-		}
-	}
-
-	/* Dependencies */
-
-	public void setUriIndex(final UriIndex uriIndex) {
-		Assert.notNull(uriIndex);
-		this.uriIndex = uriIndex;
-	}
-
-	public void setUnavailableWebScript(final WebScript unavailableWebScript) {
-		Assert.notNull(unavailableWebScript);
-		this.unavailableWebScript = unavailableWebScript;
-	}
-
-	/* Configuration */
-
-	public void setUriIndexBeanName(final String uriIndexBeanName) {
-		Assert.hasText(uriIndexBeanName);
-		this.uriIndexBeanName = uriIndexBeanName;
-	}
+    /* Configuration */
+    public void setUriIndexBeanName(final String uriIndexBeanName) {
+        Assert.hasText(uriIndexBeanName);
+        this.uriIndexBeanName = uriIndexBeanName;
+    }
 
 }
