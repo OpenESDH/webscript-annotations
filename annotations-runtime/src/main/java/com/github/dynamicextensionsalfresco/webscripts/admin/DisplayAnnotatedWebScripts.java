f9a5927210f9f61e52621f817d909a664bc2f6e4
Arnas Petraitis <arnas.petraitis@visma.com>
2016-04-15 16:52:23 +0300

    listing webscripts without family

--- annotations-runtime/src/main/java/com/github/dynamicextensionsalfresco/webscripts/admin/DisplayAnnotatedWebScripts.java
+++ annotations-runtime/src/main/java/com/github/dynamicextensionsalfresco/webscripts/admin/DisplayAnnotatedWebScripts.java
@@ -3,9 +3,13 @@ package com.github.dynamicextensionsalfresco.webscripts.admin;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.extensions.webscripts.Description;
 import org.springframework.stereotype.Component;
 
 import com.github.dynamicextensionsalfresco.webscripts.WebScriptUriRegistry;
@@ -17,31 +21,45 @@ import com.github.dynamicextensionsalfresco.webscripts.annotations.UriVariable;
 import com.github.dynamicextensionsalfresco.webscripts.annotations.WebScript;
 
 @Component
-@WebScript(families={"Admin tools"}, description="Displays annotations based Web Scripts (all and by family)")
-public class DisplayAnnotatedWebScripts{
+@WebScript(families = {"Admin tools"}, description = "Displays annotations based Web Scripts (all and by family)")
+public class DisplayAnnotatedWebScripts {
+
+    private final Logger logger = LoggerFactory.getLogger(DisplayAnnotatedWebScripts.class);
 
     @Autowired
     private WebScriptUriRegistry webScriptUriRegistry;
 
-    @Authentication(value=AuthenticationType.ADMIN)
+    @Authentication(value = AuthenticationType.ADMIN)
     @Uri(method = HttpMethod.GET, value = "/annotatedws/family/{family}", defaultFormat = "html")
-    public Map<String, Object> getWebScriptsByFamily(@UriVariable final String family){
-        final Map<String, Object> model = new HashMap<String, Object>();
-        
+    public Map<String, Object> getWebScriptsByFamily(@UriVariable final String family) {
+        final Map<String, Object> model = new HashMap<>();
+
         List<org.springframework.extensions.webscripts.WebScript> familyWebScripts = webScriptUriRegistry.getWebScripts()
                 .stream()
-                .filter(webScript -> webScript.getDescription().getFamilys().contains(family))
+                .filter(webScript -> {
+                    Description description = webScript.getDescription();
+                    if (description == null) {
+                        logger.warn("Webscript description is not defined for: {}" + webScript.toString());
+                        return false;
+                    }
+                    Set<String> familys = description.getFamilys();
+                    if (familys == null) {
+                        logger.warn("Webscript familly is not defined for: {}" + description.getScriptPath());
+                        return false;
+                    }
+                    return familys.contains(family);
+                })
                 .collect(Collectors.toList());
-        
+
         model.put("family", family);
         model.put("webScripts", familyWebScripts);
         return model;
     }
-    
-    @Authentication(value=AuthenticationType.ADMIN)
+
+    @Authentication(value = AuthenticationType.ADMIN)
     @Uri(method = HttpMethod.GET, value = "/annotatedws/all", defaultFormat = "html")
-    public Map<String, Object> getAllWebScripts(){
-        final Map<String, Object> model = new HashMap<String, Object>();
+    public Map<String, Object> getAllWebScripts() {
+        final Map<String, Object> model = new HashMap<>();
         model.put("webScripts", webScriptUriRegistry.getWebScripts());
         return model;
     }
